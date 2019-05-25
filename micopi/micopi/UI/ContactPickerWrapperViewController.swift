dce5e44be9da267ce0ac9947c591b0017e81d58d
Michel Sievers <michel@easy-target.eu>
2019-05-25 22:53:15 +0200

    Added UI dir.

---
+++ micopi/micopi/UI/ContactPickerWrapperViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactPickerWrapperViewController.swift
+//  micopi
+//
+//  Created by Michel Sievers on 25.05.19.
+//  Copyright © 2019 Things I Like GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ContactPickerWrapperViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ micopi/micopi/UI/MenuViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showContactListForGenerator()
+    }
+
+    fileprivate func showContactListForGenerator() {
+        
+    }
+}
+
