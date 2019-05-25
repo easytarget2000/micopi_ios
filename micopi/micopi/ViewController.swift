42c914df7f495ecfccb5398e36558d94f6e6f348
Michel Sievers <michel@easy-target.eu>
2019-05-25 22:42:25 +0200

    Created empty project.

---
+++ micopi/micopi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  micopi
+//
+//  Created by Michel Sievers on 25.05.19.
+//  Copyright © 2019 Things I Like GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
