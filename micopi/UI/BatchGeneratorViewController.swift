2b3b92bcf89b0b39dede5b6caea49b4096e239ed
Michel Sievers <michel@easy-target.eu>
2019-05-27 23:54:26 +0200

    Added BadgeGenerator NC and VC.

---
+++ micopi/UI/BatchGeneratorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BatchGeneratorViewController.swift
+//  micopi
+//
+//  Created by Mitch on 27.05.19.
+//  Copyright © 2019 Things I Like GmbH. All rights reserved.
+//
+
+import UIKit
+
+class BatchGeneratorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
